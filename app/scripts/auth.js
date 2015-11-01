21f6203036046c3eab038f46dbc5c0984ccbb271
Joseph Kahn <josephkahn@college.harvard.edu>
2015-11-01 16:57:03 -0500

    basic auth framework added

---
+++ app/scripts/auth.js
@@ -0,0 +1,22 @@
+var ref = new Firebase("https://bdci.firebaseio.com/");
+
+function Login(email, password) {
+  ref.authWithPassword({
+    email    : email,
+    password : password
+  }, function(error, authData) {
+    if (error) {
+      console.log("Login Failed!", error);
+    } else {
+      console.log("Authenticated successfully with payload:", authData);
+      localStorage.setItem("bdci-auth-token", authData);
+    }
+  });
+}
+
+$('#login').submit(function(e) {
+  e.preventDefault();
+  // console.log(e);
+  Login($('#InputEmail').val(), $('#InputPassword').val());
+  // console.log(localStorage.getItem('bdci-auth-token'));
+});
